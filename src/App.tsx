8c52555f9e50c7fb5365400ed993ce6c6fa82078
Pablo Llanes <pllanesparamo@gmail.com>
2024-12-05 13:13:35 -0500

    Start portfolio project W react + vite setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <h1>Portfolio</h1>
+            <p>Checkout the things I've built throughout the years.</p>
+        </>
+    );
+};
+
+export default App;
