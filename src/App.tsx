782e0a5d828000c92d59a924499dc88e8af679ee
Pablo Llanes <pllanesparamo@gmail.com>
2024-12-06 14:02:16 -0500

    Create portfolio project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import Project from './components/Project';
+
 import './App.css';
 
 const App = () => {
     return (
         <>
-            <h1>Portfolio</h1>
-            <p>Checkout the things I've built throughout the years.</p>
+            <main>
+                <header>
+                    <h1>Portfolio</h1>
+                    <p>Checkout the things I've built throughout the years.</p>
+                </header>
+                <div className='projects'>
+                    <Project key={1} number={1} />
+                    <Project key={2} number={2} />
+                    <Project key={3} number={3} />
+                    <Project key={4} number={4} />
+                    <Project key={5} number={5} />
+                    <Project key={6} number={6} />
+                    <Project key={7} number={7} />
+                    <Project key={8} number={8} />
+                    <Project key={9} number={9} />
+                </div>
+            </main>
         </>
     );
 };
---
+++ src/components/Project.tsx
@@ -0,0 +1,10 @@
+const Project = ({ number }) => {
+    return (
+        <div className='project'>
+            <div className='project-title'>Project {number}</div>
+            <div className='project-image'>[IMAGE]</div>
+        </div>
+    );
+};
+
+export default Project;
