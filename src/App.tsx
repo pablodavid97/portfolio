39762a5ce970a3c60f33c4773195333862511437
Pablo Llanes <pllanesparamo@gmail.com>
2024-12-30 13:35:04 -0500

    Add dynamic animation for each project and included proper href and images

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,164 @@ const App = () => {
                         key={1}
                         title='Tanstack Events'
                         url='https://tanstack-events-project.vercel.app'
-                        image='image.png'
+                        image='tanstack-events.png'
                     />
-                    <Project key={2} />
-                    <Project key={3} />
-                    <Project key={4} />
-                    <Project key={5} />
-                    <Project key={6} />
-                    <Project key={7} />
-                    <Project key={8} />
-                    <Project key={9} />
+                    <Project
+                        key={6}
+                        title='Next Level Food'
+                        url='https://next-foodies-project-rust.vercel.app/'
+                        image='next-level-food.png'
+                    />
+                    <Project
+                        key={2}
+                        title='Travel Agency'
+                        url='https://travel-project-ashy.vercel.app/'
+                        image='travel-agency.png'
+                    />
+                    <Project
+                        key={10}
+                        title='PlacePicker'
+                        url='https://place-picker-app-delta.vercel.app/'
+                        image='place-picker.png'
+                    />
+                    <Project
+                        key={18}
+                        title='ReactQuiz'
+                        url='https://react-quiz-app-one-orpin.vercel.app/'
+                        image='react-quiz.png'
+                    />
+                    <Project
+                        key={13}
+                        title='Final Countdown'
+                        url='https://final-countdown-five.vercel.app/'
+                        image='final-countdown.png'
+                    />
+                    <Project
+                        key={15}
+                        title='Investment Calculator'
+                        url='https://investment-calculator-five-navy.vercel.app/'
+                        image='investment-calculator.png'
+                    />
+                    <Project
+                        key={9}
+                        title='React Challenges'
+                        url='https://framer-motion-challenge-project.vercel.app/'
+                        image='framer-motion-challenge.png'
+                    />
+                    <Project
+                        key={8}
+                        title='Events Authentication'
+                        url='https://authentication-app-two-eta.vercel.app/'
+                        image='authentication-app.png'
+                    />
+                    <Project
+                        key={3}
+                        title='React TS Todo'
+                        url='https://react-ts-todo-indol.vercel.app/'
+                        image='react-ts-todo.png'
+                    />
+                    <Project
+                        key={4}
+                        title='Server Components'
+                        url='https://react-server-components-peach.vercel.app/'
+                        image='react-server-components.png'
+                    />
+                    <Project
+                        key={5}
+                        title='Redux Hooks'
+                        url='https://react-redux-hooks.vercel.app/'
+                        image='react-redux-hooks.png'
+                    />
+                    <Project
+                        key={7}
+                        title='Next Routes'
+                        url='https://next-routes-project.vercel.app/'
+                        image='next-routes.png'
+                    />
+                    <Project
+                        key={10}
+                        title='Motion Playground'
+                        url='https://framer-motion-playground-rho.vercel.app/'
+                        image='framer-motion-playground.png'
+                    />
+                    <Project
+                        key={11}
+                        title='SPA Events'
+                        url='https://spa-events-app.vercel.app/'
+                        image='spa-events.png'
+                    />
+                    <Project
+                        key={12}
+                        title='Redux Shopping'
+                        url='https://redux-shopping-app-livid.vercel.app/'
+                        image='redux-shopping-cart.png'
+                    />
+                    <Project
+                        key={13}
+                        title='Redux Auth'
+                        url='https://redux-authentication-three.vercel.app/'
+                        image='redux-auth.png'
+                    />
+                    <Project
+                        key={14}
+                        title='Redux Counter'
+                        url='https://redux-counter-steel.vercel.app/'
+                        image='redux-counter.png'
+                    />
+                    <Project
+                        key={15}
+                        title='ReactFood'
+                        url='https://food-order-app-seven-jade.vercel.app/'
+                        image='react-food.png'
+                    />
+                    <Project
+                        key={16}
+                        title='React Counter'
+                        url='https://counter-project-omega.vercel.app/'
+                        image='react-counter.png'
+                    />
+                    <Project
+                        key={17}
+                        title='React Forms'
+                        url='https://react-form-actions.vercel.app/'
+                        image='react-forms.png'
+                    />
+                    <Project
+                        key={11}
+                        title='React Shopping'
+                        url='https://react-shopping-app-seven.vercel.app/'
+                        image='react-shopping.png'
+                    />
+                    <Project
+                        key={12}
+                        title='Project Manager'
+                        url='https://react-project-manager-app.vercel.app/'
+                        image='react-project-manager.png'
+                    />
+                    <Project
+                        key={14}
+                        title='ReactArt'
+                        url='https://react-art-project.vercel.app/'
+                        image='react-art.png'
+                    />
+                    <Project
+                        key={16}
+                        title='Tic Tac Toe'
+                        url='https://react-tic-tac-toe-two-snowy.vercel.app/'
+                        image='tic-tac-toe.png'
+                    />
+                    <Project
+                        key={17}
+                        title='React Essentials'
+                        url='https://react-components-black-rho.vercel.app/'
+                        image='react-essentials.png'
+                    />
+                    <Project
+                        key={18}
+                        title='React Blog'
+                        url='https://react-blog-app-tau-self.vercel.app/'
+                        image='react-blog-app.png'
+                    />
                 </div>
             </main>
         </>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,47 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 const Project = ({ title = '', url = '#', image = '' }) => {
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    const handleMouseMove = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        setMousePosition({ x, y });
+
+        setDimensions({
+            width: rect.width,
+            height: rect.height,
+        });
+    };
+
+    const calculateTransform = () => {
+        const { x, y } = mousePosition;
+        const { width, height } = dimensions;
+
+        if (width === 0 || height === 0)
+            return { rotateX: 0, rotateY: 0, translateZ: 0 };
+
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        const normalizedX = (x - centerX) / centerX;
+        const normalizedY = (y - centerY) / centerY;
+
+        const rotateX = normalizedY * 15;
+        const rotateY = normalizedX * 15;
+
+        // Calculate the depth (Z-axis) based on distance from the center
+        const distanceToCenter = Math.sqrt(normalizedX ** 2 + normalizedY ** 2);
+        const translateZ = 15 - distanceToCenter * 30; // Increase or decrease this for the pulling effect
+
+        return { rotateX, rotateY, translateZ };
+    };
+
+    const { rotateX, rotateY, translateZ } = calculateTransform();
+
     const imageStyle = {
         backgroundImage: `url(${
             import.meta.env.VITE_PUBLIC_URL
@@ -8,12 +51,35 @@ const Project = ({ title = '', url = '#', image = '' }) => {
         backgroundRepeat: 'no-repeat',
     };
     return (
-        <a className='project' href={url} target={url} style={imageStyle}>
+        <motion.a
+            className='project'
+            onMouseMove={handleMouseMove}
+            onMouseLeave={() =>
+                setMousePosition({
+                    x: dimensions.width / 2,
+                    y: dimensions.height / 2,
+                })
+            }
+            animate={{
+                rotateX,
+                rotateY,
+                z: translateZ,
+            }}
+            whileHover={{ scale: 1.15 }} // Apply scale on hover to emphasize the pulling effect
+            transition={{
+                type: 'spring',
+                stiffness: 100,
+                damping: 20,
+            }}
+            href={url}
+            target={url}
+            style={imageStyle}
+        >
             <div className='project-meta'>
                 <h3 className='project-title'>{title}</h3>
                 <div className='divider'></div>
             </div>
-        </a>
+        </motion.a>
     );
 };
 
